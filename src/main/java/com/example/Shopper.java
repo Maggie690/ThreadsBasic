3260497bfd238d6e74d67b2bcb0bfd12520e0a91
m.ivanova <margaritawork76@gmail.com>
2025-01-31 11:28:20 +0200

    add ReentrantLock to prevent date race

--- src/main/java/com/example/Shopper.java
+++ src/main/java/com/example/Shopper.java
@@ -1,12 +1,26 @@
 package com.example;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Shopper extends Thread {
     static int garlicCount = 0;
+    static Lock pencil = new ReentrantLock();
 
     @Override
     public void run() {
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 5; i++) {
+            pencil.lock();
             garlicCount++;
+            pencil.unlock();
+
+            System.out.println(Thread.currentThread().getName() + " is thinking");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+            }
+
+            System.out.println();
         }
     }
 
@@ -14,6 +28,9 @@ public class Shopper extends Thread {
         public static void main(String[] args) throws InterruptedException {
             Thread olivia = new Shopper();
             Thread maggie = new Shopper();
+
+            olivia.setName("Olivia");
+            maggie.setName("Maggie");
 
             olivia.start();
             maggie.start();
