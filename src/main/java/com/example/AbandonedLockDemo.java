22e6ffda59a1bacbde96ae2127d796165cdb9b79
m.ivanova <margaritawork76@gmail.com>
2025-01-31 14:01:45 +0200

    AbandonedLockDemo

---
+++ src/main/java/com/example/AbandonedLockDemo.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Philosopher extends Thread {
+
+    private Lock firstChopstick, secondChopstick;
+    private static int sushiCount = 50;
+
+    public Philosopher(String name, Lock firstChopstick, Lock secondChopstick) {
+        this.setName(name);
+        this.firstChopstick = firstChopstick;
+        this.secondChopstick = secondChopstick;
+    }
+
+    public void run() {
+        int eatenSushi = 0;
+        while (sushiCount > 0) { // eat sushi until it's all gone
+
+            // pick up chopsticks
+            firstChopstick.lock();
+            secondChopstick.lock();
+
+            // take a piece of sushi
+            if (sushiCount > 0) {
+                sushiCount--;
+                eatenSushi++;
+                System.out.println(this.getName() + " took a piece! Sushi remaining: " + sushiCount);
+            }
+
+            // put down chopsticks
+            secondChopstick.unlock();
+            firstChopstick.unlock();
+        }
+
+        System.out.println(this.getName() + "was able to eat " + eatenSushi);
+    }
+}
+
+public class AbandonedLockDemo {
+    public static void main(String[] args) {
+        Lock chopstickA = new ReentrantLock();
+        Lock chopstickB = new ReentrantLock();
+        Lock chopstickC = new ReentrantLock();
+        new Philosopher("Barron", chopstickA, chopstickB).start();
+        new Philosopher("Olivia", chopstickB, chopstickC).start();
+        new Philosopher("Steve", chopstickA, chopstickC).start();
+    }
+}
